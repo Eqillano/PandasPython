f9e34454bccb15ec25394b8bdbdc85f64a4df1c8
Sergey Vanteev <fukkrypto@gmail.com>
2020-06-04 12:10:16 +0300

    добавлены комментарии

--- 2.4-2.6/task24-25.py
+++ 2.4-2.6/task24-25.py
@@ -1,25 +1,25 @@
 import pandas as pd
 
 
-df = pd.read_json('TestJson/small_data_contracts.json')
-#print(df.head())
+df = pd.read_json('json/small_data_contracts.json')
+
 ''' Задание 2.4 '''
 
-''' трансформируем два столбца что-бы можно было с ними проводить вычисление'''
 
+# меняем тип данных что-бы можно было проводить с ними вычисления
 df['From'] = pd.to_datetime(df['From'])
 df['To'] = pd.to_datetime(df['To'])
 
 
-''' cоздаём столбец что-бы просчитать время '''
-
+#cоздаём столбец что-бы просчитать разницу во времени
 df['duration'] = df['To'] - df['From']
 df1 = df[df['duration'] >= '00:05:00']
 
 
-''' группируем по member_id и считаем кол-во контактов в убывающем порядке '''
+#группируем по member_id и считаем кол-во контактов,сортируем в убывающем порядке
 
-df1 = df1.groupby(['Member1_ID']).size().to_frame('count').reset_index().sort_values(['count'], ascending=[True])
+df1 = df1.groupby(['Member1_ID']).size().to_frame('count').reset_index().sort_values(['count'], ascending=[False])
+
 
 
 
@@ -28,6 +28,5 @@ df1 = df1.groupby(['Member1_ID']).size().to_frame('count').reset_index().sort_va
 Вывести список людей, отсортированный в обратном порядке по общей длительности
 контакта с другими людьми. '''
 
-
+# группируем по Member1_ID и суммируем длинну котнтакт , сортирем по длине контакта и выводим в обратном порядке
 df2 = df.groupby(['Member1_ID'])['duration'].sum().to_frame('sum_duration').reset_index().sort_values(['sum_duration'],ascending=[False])
-print(df2)
--- 2.4-2.6/task26.py
+++ 2.4-2.6/task26.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
-df = pd.read_json('TestJson/small_data_contracts.json')
-df2 = pd.read_json('TestJson/small_data_persons.json')
+
+df = pd.read_json('json/small_data_contracts.json')
+df2 = pd.read_json('json/small_data_persons.json')
 
 #print(df.head())
 #print(df2.head())
@@ -13,24 +14,29 @@ df2 = pd.read_json('TestJson/small_data_persons.json')
 Найти возрастную группу людей, которая имеет наиболее частый контакт с другими
 людьми. Так же, как и в задании 2.1 контактом считается контакт длительностью 5 и более'''
 
-
+# соединяем две талблицы используя inner join
 dfx = pd.merge(df,df2,left_on='Member1_ID',right_on='ID',how='inner')
 
+#меняем тип данных что-бы можно было проводить с ними вычисления
 dfx['From'] = pd.to_datetime(df['From'])
 dfx['To'] = pd.to_datetime(df['To'])
 
+#создаём колонку длительность контакта
 dfx['duration'] = dfx['To'] - dfx['From']
 
+# условеия что-бы контакт был больше двух минут
 dfx = dfx[dfx['duration'] >= '00:05:00']
 
+# убираем колонку
 dfx.drop(['Member2_ID'],axis=1,inplace=True)
 
+# создаём группы людей
 dfx.loc[(dfx.Age > 18) ,  'Возрастная Группа'] = '18-30'
 dfx.loc[(dfx.Age > 30),  'Возрастная Группа'] = '30-50'
 dfx.loc[(dfx.Age > 50),  'Возрастная Группа'] = '50-70'
-dfx.loc[(dfx.Age > 70),  'Возрастная Группа'] = '70-90'
+dfx.loc[(dfx.Age > 70),  'Возрастная Группа'] = '> 70'
 
 
-
+# группируем по возрстной группе и считаем общее кол-во людей в общей группе,сортируем в убывающем порядке
 dfxx = dfx.groupby(['Возрастная Группа'])['Member1_ID'].count().to_frame('кол-во').reset_index().sort_values(['кол-во'],ascending=[False])
-print(dfxx)
+#Наиболее частый контакт имеет группа 18-30 лет
